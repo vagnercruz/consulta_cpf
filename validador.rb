ac48e80918aa7324379f83a882531fbd6e9c952f
vagnercruz <vagnerdacruz48@gmail.com>
2025-11-11 20:26:14 -0300

    ajustando o gemfile

--- validador.rb
+++ validador.rb
@@ -2,12 +2,6 @@ require 'cpf_cnpj'
 
 class ValidadorCPF
   def self.validar(cpf)
-    if CPF.valid?(cpf)
-      puts "O CPF #{cpf} é válido."
-      true
-    else
-      puts "O CPF #{cpf} é inválido."
-      false
-    end
+    CPF.valid?(cpf)
   end
 end
