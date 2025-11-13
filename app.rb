47493899742e2c0e2bc3a6cd134a67cc35022c98
vagnercruz <vagnerdacruz48@gmail.com>
2025-11-12 22:49:53 -0300

    Adicionando histórico de pesquisas

--- app.rb
+++ app.rb
@@ -1,16 +1,51 @@
 require 'sinatra'
-require_relative 'validador'
+require 'cpf_cnpj'
+require 'json'
+require 'fileutils'
 
-set :bind, '0.0.0.0'
-set :port, 4567
+HISTORICO_PATH = 'data/historico.json'
+
+FileUtils.mkdir_p('data')
+File.write(HISTORICO_PATH, '[]') unless File.exist?(HISTORICO_PATH)
+
+helpers do
+  def carregar_historico
+    JSON.parse(File.read(HISTORICO_PATH))
+  end
+
+  def salvar_historico(historico)
+    File.write(HISTORICO_PATH, JSON.pretty_generate(historico))
+  end
+end
 
 get '/' do
   erb :index
 end
 
 post '/validar' do
-  cpf = params[:cpf].to_s.strip
-  @cpf = cpf
-  @valido = ValidadorCPF.validar(cpf)
+  cpf = params[:cpf].to_s.gsub(/\D/, '')
+  @cpf_formatado = CPF.new(cpf).formatted
+  @valido = CPF.valid?(cpf)
+
+
+  historico = carregar_historico
+  historico << {
+    cpf: @cpf_formatado,
+    valido: @valido,
+    data: Time.now.strftime('%d/%m/%Y %H:%M:%S')
+  }
+  historico = historico.last(10)
+  salvar_historico(historico)
+
   erb :resultado
+end
+
+get '/historico' do
+  @historico = carregar_historico.reverse
+  erb :historico
+end
+
+post '/historico/limpar' do
+  salvar_historico([])
+  redirect '/historico'
 end
